2a168bcc0d5a6db0247c4a8859eb97f0a08dc964
rossonero4ever <osman.mehinagic16212@gmail.com>
2015-05-19 23:03:39 +0200

    Admin panel
    Omoguceno upravljanje korisnicima - admin panelu imaju pristup samo
    korisnici sa ulogom administrator

--- QuestioningSystem/QuestioningSystem/Controllers/HomeController.cs
+++ QuestioningSystem/QuestioningSystem/Controllers/HomeController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuestioningSystem.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace QuestioningSystem.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController()
+            : this(new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext())))
+        {
+        }
+
+        public HomeController(UserManager<ApplicationUser> userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public UserManager<ApplicationUser> UserManager { get; private set; }
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +41,51 @@ namespace QuestioningSystem.Controllers
 
             return View();
         }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult AdminPanel()
+        {
+            List<ApplicationUser> users = new List<ApplicationUser>();
+            using (var context = ApplicationDbContext.Create())
+            {
+                users = context.Users.ToList();
+            }
+            return View(users);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Ban(string id)
+        {
+            using (var context = ApplicationDbContext.Create())
+            {
+                var user = context.Users.Find(id);
+                if (user != null)
+                {
+                    user.Banned = true;
+                    context.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("AdminPanel");
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult UnBan(string id)
+        {
+            using (var context = ApplicationDbContext.Create())
+            {
+                var user = context.Users.Find(id);
+                if (user != null)
+                {
+                    user.Banned = false;
+                    context.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("AdminPanel");
+        }
+
+
+
     }
 }
